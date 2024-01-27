34ea0a7f4db537c01d2f551a0aa1dbc52c10a729
peakBreaker <andershurum@gmail.com>
2024-01-27 18:31:13 +0100

    updated birthdat scene with timers and logic

---
+++ scripts/GameController.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+@onready var anim_sprite = $AnimatedSprite3D
+var can_intervene = true
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim_sprite.play("waiting")
+	$shake_speed.play("shake_speed")
+	# Do some action
+	await get_tree().create_timer(5.0).timeout
+	# Do something afterwards
+	anim_sprite.play("inhale")
+	await get_tree().create_timer(5.0).timeout
+	$Instruction.queue_free()
+	can_intervene = false
+	anim_sprite.play("waiting")
+	
+func _input(event):
+	# handle the space button here
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_SPACE:
+			print("SPACE was pressed")
+			anim_sprite.play("crying")
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
