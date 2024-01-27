0d4c99d8763ff72968514060b894c23c5e11d3e3
TerjeBallestad <terj.ba@gmail.com>
2024-01-27 18:16:22 +0100

    changes

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,23 +1,43 @@
 extends Node3D
 
+var active_scene := 1
+
 var scenes = [
-	preload("res://scenes/sm_cake_scene_combined.tscn")
+	preload("res://scenes/sm_cake_scene_combined.tscn"),
+	preload("res://scenes/01_birthday.tscn"),
+	preload("res://scenes/02_teeth.tscn"),
+	preload("res://scenes/03_lake.tscn"),
+	preload("res://scenes/04_nine_eleven.tscn"),
+	preload("res://scenes/05_pond.tscn"),
+	preload("res://scenes/06_well.tscn"),
+	preload("res://scenes/07_basement.tscn")
 ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
+
 func loadScene(scene_idx: int):
 	var scene = scenes[scene_idx]
 	var instance = scene.instantiate()
+	instance.connect('on_win', on_win)
+	instance.connect('on_lose', on_lose)
 	add_child(instance)
+	if scene_idx > 0:
+		pass
+		# scenes[scene_idx - 1].queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
-
 func _on_play_button_pressed():
-	loadScene(0)
+	active_scene += 1
+	loadScene(active_scene)
 	$Control.hide()
+
+func on_win():
+	print('won')
+
+func on_lose():
+	print('lost')
