9b84cdef7b19ae298f5b40c1e229aeb47093f24f
peakBreaker <andershurum@gmail.com>
2024-01-28 15:22:19 +0100

    enh: sound update birthday and clown

--- scenes/01_birthday.gd
+++ scenes/01_birthday.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var anim_sprite = $Game/AnimatedSprite3D
 @onready var sound_player = $Game/sounds
+@onready var anim_candle = $Game/candle
 
 signal on_win
 signal on_lose
@@ -10,6 +11,7 @@ var game_over = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	anim_candle.play("default")
 	anim_sprite.play("waiting")
 	$Game/shake_speed.play("shake_speed")
 	# Do some action
@@ -22,11 +24,13 @@ func _ready():
 		# Do something afterwards
 		anim_sprite.play("blowing")
 	await get_tree().create_timer(2.0).timeout
+	anim_candle.queue_free()
+	await get_tree().create_timer(2.0).timeout
 	if not game_over:
 		on_win.emit()
 		game_over = true
 		anim_sprite.play("waiting")
-		sound_player.stream = preload("res://assets/sounds/candle_blowout.mp3")
+		sound_player.stream = preload("res://assets/sounds/BlowoutMP3.mp3")
 		sound_player.play()
 
 func _input(event):
@@ -35,7 +39,11 @@ func _input(event):
 		$Game/Instruction.queue_free()
 		game_over = true
 		$background_sounds.stop()
-
+		await get_tree().create_timer(2.0).timeout
+		sound_player.stream = preload("res://assets/sounds/BlowoutMP3.mp3")
+		sound_player.play()
+		await get_tree().create_timer(1.0).timeout		
+		anim_candle.queue_free()
 		await get_tree().create_timer(2.0).timeout
 		on_lose.emit()
 		anim_sprite.play("crying")
