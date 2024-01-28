864c82721ff977e3de2c67bb29ef560aacafc44d
peakBreaker <andershurum@gmail.com>
2024-01-28 12:25:22 +0100

    enh: added basic jfk scene

--- scenes/08_jfk.gd
+++ scenes/08_jfk.gd
@@ -1,8 +1,25 @@
 extends Node3D
 
+@onready var jfk_anim_sprite = $Game/AnimatedSprite3D
+@onready var wife_anim_sprite = $Game/wife_AnimatedSprite3D
+@onready var sound_player = $Game/sounds
+
 signal on_win
 signal on_lose
 
+var game_over = false
+
+func _ready():
+	jfk_anim_sprite.play("waving")
+	wife_anim_sprite.play("waving")
+	$Game/Instruct/shake_speed.play("shake_speed")
+	# Do some action
+	await get_tree().create_timer(15.0).timeout
+	if not game_over:
+		on_win.emit()
+		$Game/Instruct.queue_free()
+		game_over = true
+
 func _input(event):
-    if event.is_action_pressed("the_button"):
-        on_lose.emit()
+	if event.is_action_pressed("the_button"):
+		on_lose.emit()
