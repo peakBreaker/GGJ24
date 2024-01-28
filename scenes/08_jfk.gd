93f6690340d0f4860ae5d3660334e7858bd8a9e0
peakBreaker <andershurum@gmail.com>
2024-01-28 14:13:26 +0100

    enh: jfk done

--- scenes/08_jfk.gd
+++ scenes/08_jfk.gd
@@ -14,12 +14,26 @@ func _ready():
 	wife_anim_sprite.play("waving")
 	$Game/Instruct/shake_speed.play("shake_speed")
 	# Do some action
-	await get_tree().create_timer(15.0).timeout
+	await get_tree().create_timer(5.0).timeout
 	if not game_over:
-		on_win.emit()
 		$Game/Instruct.queue_free()
+	await get_tree().create_timer(5.0).timeout
+	if not game_over:
 		game_over = true
+		# TODO: ADD SHOOT sound here
+		jfk_anim_sprite.play("dead")
+		wife_anim_sprite.play("shock")
+		#$WorldEnvironment.environment
+		await get_tree().create_timer(2.0).timeout
+		var env = $WorldEnvironment.get_environment()
+		$scene_president/AnimationTree.active = false
+		$Game/AnimatedSprite3D/AnimationPlayer.stop()
+		env.set_adjustment_saturation(0.0)
+		on_lose.emit()
 
 func _input(event):
 	if event.is_action_pressed("the_button"):
-		on_lose.emit()
+		game_over = true
+		on_win.emit()
+		$Control/Sprite2D.show()
+		# make hand appear
